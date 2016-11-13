eb3bc0f8901dd151f5eaca1ed5a7c24b04c37e60
ciscoo <cisco@mateo.io>
2016-11-13 14:43:30 -0600

    Parse human address from xml; Add missing date fields. Fix type casting for emergency services.

--- database/seeds/HospitalTableSeeder.php
+++ database/seeds/HospitalTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class HospitalTableSeeder extends Seeder
 {
@@ -12,9 +12,9 @@ class HospitalTableSeeder extends Seeder
      */
     public function run()
     {
-        $xml = $this->loadXML();
-    
-        foreach($xml->row[0] as $hospital) {
+        $hospitals = $this->loadXML();
+
+        foreach($hospitals as $hospital) {
             DB::table('hospitals')->insert([
                 'provider_id' => $hospital->provider_id,
                 'name' => $hospital->hospital_name,
@@ -26,21 +26,40 @@ class HospitalTableSeeder extends Seeder
                 'phone' => $hospital->phone_number['phone_number'],
                 'type' => $hospital->hospital_type,
                 'ownership' => $hospital->hospital_ownership,
-                'emergency_services' => (int) $hospital->emergency_services,
+                'emergency_services' => (boolean) $hospital->emergency_services,
+                'human_address' => $this->parseHumanAddress($hospital->location['human_address']),
                 'latitude' => $hospital->location['latitude'],
-                'longitude' => $hospital->location['longitude']
+                'longitude' => $hospital->location['longitude'],
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);
         }
     }
 
+    /**
+     * Attempts to load the hospital xml file.
+     *
+     * @return null|SimpleXMLElement
+     */
     public function loadXML() : ?SimpleXMLElement
     {
         $path = resource_path('assets/database/ushospitals.xml');
+        return file_exists($path) ? simplexml_load_file($path)->row[0] : null;
+    }
 
-        if (file_exists($path)) {
-            return simplexml_load_file($path);
-        } else {
-            return null;
-        }
+    /**
+     * The human address from the provided hospital xml is JSON encoded.
+     * This method will decode the JSON and return the values separated
+     * by a single comma.
+     *
+     * @param string $humanAddress - The JSON encoded human address
+     * @return string - The decoded human address
+     */
+    public function parseHumanAddress(string $humanAddress) : string
+    {
+        $decodedAddress = json_decode($humanAddress, true);
+        $address = implode(", ", $decodedAddress);
+
+        return $address;
     }
 }
