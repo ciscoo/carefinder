4d81453eff13df533c545479ec509bc10465d6f6
ciscoo <cisco21c@gmail.com>
2016-12-05 16:24:38 -0600

    Protect all routes where needed.

--- routes/api.php
+++ routes/api.php
@@ -13,54 +13,43 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('/hospitals', 'HospitalController@index');
-    Route::post('/hospitals', 'HospitalController@createHospital');
-    Route::delete('/hospitals', 'HospitalController@deleteAllHospitals');
-
-    Route::get('/hospitals/id/{id}', 'HospitalController@show');
-    Route::put('/hospitals/id/{id}', 'HospitalController@updateOrCreate');
-    Route::delete('/hospitals/id/{id}', 'HospitalController@deleteById');
-
-    Route::get('/hospitals/city/{name}', 'HospitalController@showByCity');
-    Route::delete('/hospitals/city/{name}', 'HospitalController@deleteByCity');
-
-    Route::get('/hospitals/state/{name}', 'HospitalController@showByState');
-    Route::delete('/hospitals/state/{name}', 'HospitalController@deleteByState');
-
-    Route::get('/hospitals/county/{name}', 'HospitalController@showByCounty');
-    Route::delete('/hospitals/county/{name}', 'HospitalController@deleteByCounty');
-
-    Route::get('/hospitals/statecity/{state}/{city}', 'HospitalController@showByStateCity');
-    Route::delete('/hospitals/statecity/{state}/{city}', 'HospitalController@deleteByStateCity');
-
-    Route::get('/hospitals/name/{name}', 'HospitalController@showByName');
-    Route::delete('/hospitals/name/{name}', 'HospitalController@deleteByName');
-
-    Route::get('/hospitals/type/{type}', 'HospitalController@showbyType');
-    Route::delete('/hospitals/type/{type}', 'HospitalController@deleteByType');
-
-    Route::get('/hospitals/ownership/{owner}', 'HospitalController@showbyOwner');
-    Route::delete('/hospitals/ownership/{owner}', 'HospitalController@deletebyOwner');
-
-    Route::get('/hospitals/emergency/{emergency}', 'HospitalController@showbyEmergency');
-    Route::delete('/hospitals/emergency/{emergency}', 'HospitalController@deletebyEmergency');
-
-    Route::get('/hospitals/latlon/{latitude}/{longitude}/{distance}', 'HospitalController@showbyDistance');
-    Route::delete('/hospitals/latlon/{latitude}/{longitude}/{distance}', 'HospitalController@deletebyDistance');
-});
-
 // Unprotected routes
 Route::group(['middleware' => 'api'], function () {
-    Route::get('/keys', 'KeyController@index');
     Route::get('/key', 'KeyController@get');
-
     Route::post('/key/{key}', 'KeyController@save');
+    Route::post('/key/{key}/{level}', 'KeyController@createLevel');    
+    
+    Route::get('/hospitals', 'HospitalController@index');
+    Route::get('/hospitals/id/{id}', 'HospitalController@show');
+    Route::get('/hospitals/city/{name}', 'HospitalController@showByCity');
+    Route::get('/hospitals/state/{name}', 'HospitalController@showByState');
+    Route::get('/hospitals/county/{name}', 'HospitalController@showByCounty');
+    Route::get('/hospitals/statecity/{state}/{city}', 'HospitalController@showByStateCity');
+    Route::get('/hospitals/name/{name}', 'HospitalController@showByName');
+    Route::get('/hospitals/type/{type}', 'HospitalController@showbyType');
+    Route::get('/hospitals/ownership/{owner}', 'HospitalController@showbyOwner');
+    Route::get('/hospitals/emergency/{emergency}', 'HospitalController@showbyEmergency');
+    Route::get('/hospitals/latlon/{latitude}/{longitude}/{distance}', 'HospitalController@showbyDistance');
+    
+});
+
+// Protected routes
+Route::group(['middleware' => ['api', 'level']], function () {
+    Route::get('/keys', 'KeyController@index');
     Route::delete('/key/{key}', 'KeyController@delete');
     Route::get('/key/{key}', 'KeyController@index');
-    Route::post('/key/{key}/{level}', 'KeyController@createLevel');
+
+    Route::post('/hospitals', 'HospitalController@createHospital');
+    Route::put('/hospitals/id/{id}', 'HospitalController@updateOrCreate');
+    Route::delete('/hospitals', 'HospitalController@deleteAllHospitals');    
+    Route::delete('/hospitals/id/{id}', 'HospitalController@deleteById');
+    Route::delete('/hospitals/city/{name}', 'HospitalController@deleteByCity');
+    Route::delete('/hospitals/state/{name}', 'HospitalController@deleteByState');
+    Route::delete('/hospitals/county/{name}', 'HospitalController@deleteByCounty');
+    Route::delete('/hospitals/statecity/{state}/{city}', 'HospitalController@deleteByStateCity');
+    Route::delete('/hospitals/name/{name}', 'HospitalController@deleteByName');
+    Route::delete('/hospitals/type/{type}', 'HospitalController@deleteByType');
+    Route::delete('/hospitals/ownership/{owner}', 'HospitalController@deletebyOwner');
+    Route::delete('/hospitals/emergency/{emergency}', 'HospitalController@deletebyEmergency');
+    Route::delete('/hospitals/latlon/{latitude}/{longitude}/{distance}', 'HospitalController@deletebyDistance');
 });
