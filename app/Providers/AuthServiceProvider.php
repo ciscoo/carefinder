fa81d195c7ca6070a4ed374f57981d9d16539338
ciscoo <cisco21c@gmail.com>
2016-12-05 20:07:03 -0600

    Remove Passport as we won’t be using it anymore.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Carbon\Carbon;
-use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -26,10 +24,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Passport::routes();
-        Passport::pruneRevokedTokens();
-        Passport::tokensExpireIn(Carbon::now()->addDays(15));
-        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
